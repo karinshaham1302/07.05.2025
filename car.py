c3caf3f22c4a125d5435744b49a5e2c3c92665e2
karin132025! <karinshaham1302@>
2025-05-11 17:07:48 +0300

    upload

---
+++ car.py
@@ -0,0 +1,9 @@
+class Car:
+    type = 'Toyota'
+    midel = 'RAV-4'
+    color = 'Black'
+    speed = 180
+
+
+if __name__ == '__main__':
+    print(Car.type)
---
+++ main.py
@@ -0,0 +1,16 @@
+import car
+
+print(car.Car.speed)
+
+
+
+from car import *
+
+print(Car.speed)
+
+
+
+from car import Car as C
+
+print(C.type)
+
